159b23ff36cf2a6d1ca226d2ac40435891008dcc
emvmikhailevg <144726521+emvmikhailevg@users.noreply.github.com>
2024-11-27 11:48:28 +0500

    Initial Commit

---
+++ src/main/java/ru/emelianov/gymsystemproject/GymSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.emelianov.gymsystemproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymSystemProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymSystemProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/emelianov/gymsystemproject/GymSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.emelianov.gymsystemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymSystemProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
